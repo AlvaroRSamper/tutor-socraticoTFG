018226b885d692f6094a1a2d076e3e89b13a0948
alroper04 <alvaro.rodsam@uma.es>
2026-09-15 11:30:58 +0200

    Aislamiento CRrf

--- src/test/java/es/uma/tfg/tutor_socratico/controlador/CsrfFlujoRealTests.java
+++ src/test/java/es/uma/tfg/tutor_socratico/controlador/CsrfFlujoRealTests.java
@@ -12,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -30,6 +31,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
  */
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
 class CsrfFlujoRealTests {
 
     @Autowired
